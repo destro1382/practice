340565af0113948e10d366336c804ce184b4bf98
destro1382 <destro1382@gmail.com>
2026-03-28 21:37:01 -0400

    Finished Calc for now

--- calculator.py
+++ calculator.py
@@ -1,6 +1,3 @@
-#Send error when input is not a number
-#Allow to type number out
-
 def is_num(str):
     try:
         float(str)
@@ -11,27 +8,33 @@ def is_num(str):
 
 
 print("Hello, I am a calculator!  Type what mathematical equation you would like accomplished!")
-num_one = input("Please type the first number:")
-if is_num(num_one): 
-    pass
-else:
-    print("Please enter a valid number")
+while True:
+    num_one = input("Please type the first number:")
+    if is_num(num_one): 
+        break
+    else:
+        print("Please enter a valid number")
 
+while True:
+    num_two = input("Please type the second number:")
+    if is_num(num_two):
+        break
+    else:
+        print("Please enter a valid number")
 
-num_two = input("Please type the second number:")
-operator = input("Please type what calculation you would like performed, + - * /:")
-
-if is_num(num_one): 
-    pass
-else:
-    print("Please enter a valid number")
-
-if operator.lower() == "+" or operator.lower() == "add":
-    print(float(num_one) + float(num_two))
-elif operator.lower() == "-" or operator.lower() == "subtract" :
-    print(float(num_one) - float(num_two))
-elif operator.lower() == "*" or operator.lower() == "multiply":
-    print(float(num_one) * float(num_two))
-elif operator.lower() == "/" or operator.lower() == "divide":
-    print(float(num_one) / float(num_two))
-
+while True:
+    operator = input("Please type what calculation you would like performed, + - * /:")
+    if operator.lower() == "+" or operator.lower() == "add":
+        print(float(num_one) + float(num_two))
+        break
+    elif operator.lower() == "-" or operator.lower() == "subtract" :
+        print(float(num_one) - float(num_two))
+        break
+    elif operator.lower() == "*" or operator.lower() == "multiply":
+        print(float(num_one) * float(num_two))
+        break
+    elif operator.lower() == "/" or operator.lower() == "divide":
+        print(float(num_one) / float(num_two))
+        break
+    else:
+        print("Please enter a valid operator")
